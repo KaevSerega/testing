e9f87f94773074aed9d25ba51783748c0450c2d5
KaevS <Kaev.Serega@yandex.ru>
2024-01-07 20:49:29 +0300

    Стилистические правки

--- src/test/java/org/example/QrCodePage.java
+++ src/test/java/org/example/QrCodePage.java
@@ -6,14 +6,18 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class QrCodePage {
-    public WebDriver driver;
-    public QrCodePage(WebDriver driver) {
-        PageFactory.initElements(driver, this);
-        this.driver = driver; }
+
     @FindBy(xpath = "//*[@id=\"root\"]/div/div[2]/div[2]/div/div/div[2]/div[2]/div/div/div/div[1]/div/div/span")
     private WebElement text;
+
+    public WebDriver driver;
+
+    public QrCodePage(WebDriver driver) {
+        PageFactory.initElements(driver, this);
+        this.driver = driver;
+    }
+
     public String getText() {
         return text.getText();
     }
-
 }
